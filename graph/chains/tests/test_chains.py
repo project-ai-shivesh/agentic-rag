aed80d6d939e9635bc5382857d12cecc57328822
shivesh Kumar <shiveshkumarsuri@hotmail.com>
2026-05-15 09:03:03 -0500

    document grading completed

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,2 +1,27 @@
-def test_foo() -> None:
-    assert 1 == 1
+from dotenv import load_dotenv
+load_dotenv()
+
+from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from ingestion import retriever
+
+
+def test_retrieval_grader_answer_yes():
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[0].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "yes"
+
+def test_retrieval_grader_answer_no():
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": "How to make Pizza", "document": doc_txt}
+    )
+    assert res.binary_score == "no"
